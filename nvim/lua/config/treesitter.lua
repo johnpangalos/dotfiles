d95aa4e55bf41974ebd7213b62e1a8f39b73ea1c
John Pangalos <john@pangalos.dev>
2022-04-20 08:35:55 +0200

    Remove phpdoc from treesitter

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,6 +2,8 @@ local ts_configs = require("nvim-treesitter.configs")
 
 ts_configs.setup({
 	ensure_installed = "all",
+
+  ignore_install = { "phpdoc" },
 	highlight = { enable = true, use_languagetree = true },
 	indent = { enable = false },
 	incremental_selection = {
