aa7d1cae8e51b405937739de10904653ac4b1b8f
John Pangalos <john.pangalos@pagero.com>
2024-01-29 09:13:46 +0100

    Add time and battery info to wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require("wezterm")
 
 local function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
 end
 
 local config = {}
@@ -33,87 +33,100 @@ config.webgpu_preferred_adapter = gpu
 config.leader = { key = "a", mods = "CTRL" }
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 8,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 8,
+	bottom = 0,
 }
 
 config.keys = {
-  {
-    key = "r",
-    mods = "LEADER",
-    action = wezterm.action.ActivateKeyTable({
-      name = "resize_pane",
-      timeout_milliseconds = 1000,
-    }),
-  },
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.ActivateKeyTable({
+			name = "resize_pane",
+			timeout_milliseconds = 1000,
+		}),
+	},
 
-  {
-    key = "v",
-    mods = "LEADER",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "b",
-    mods = "LEADER",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "a",
-    mods = "LEADER|CTRL",
-    action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
-  },
-  {
-    key = "h",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-  {
-    key = "k",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Up"),
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Down"),
-  },
-  { key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-  { key = "w", mods = "LEADER", action = wezterm.action.SwitchToWorkspace },
-  {
-    key = "H",
-    mods = "ALT|SHIFT",
-    action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-  },
-  {
-    key = "J",
-    mods = "ALT|SHIFT",
-    action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-  },
-  { key = "K", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
-  {
-    key = "L",
-    mods = "ALT|SHIFT",
-    action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-  },
-  {
-    key = "s",
-    mods = "LEADER",
-    action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
-  },
-  {
-    key = "z",
-    mods = "LEADER",
-    action = wezterm.action.TogglePaneZoomState,
-  },
-  { key = ">", mods = "LEADER", action = wezterm.action.RotatePanes("Clockwise") },
-  { key = "<", mods = "LEADER", action = wezterm.action.RotatePanes("CounterClockwise") },
+	{
+		key = "v",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "b",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{ key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "w", mods = "LEADER", action = wezterm.action.SwitchToWorkspace },
+	{
+		key = "H",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "J",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{ key = "K", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+	{
+		key = "L",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		key = "s",
+		mods = "LEADER",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+	},
+	{
+		key = "z",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{ key = ">", mods = "LEADER", action = wezterm.action.RotatePanes("Clockwise") },
+	{ key = "<", mods = "LEADER", action = wezterm.action.RotatePanes("CounterClockwise") },
 }
+
+wezterm.on("update-right-status", function(window, pane)
+	local date = wezterm.strftime("%a %b %-d %H:%M ")
+
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+	end
+
+	window:set_right_status(wezterm.format({
+		{ Text = bat .. "   " .. date },
+	}))
+end)
 
 return config
