0cb1fbfedd22dc4f2f213c2454dcb8ac518efd84
John Pangalos <john@pangalos.dev>
2024-07-02 10:01:32 +0200

    add eslint nvim config

--- .config/nvim/lua/plugins/utils/index.lua
+++ .config/nvim/lua/plugins/utils/index.lua
@@ -1,3 +1,5 @@
+local LazyUtil = require("lazy.core.util")
+
 local cmd = vim.cmd
 local o_s = vim.o
 local map_key = vim.api.nvim_set_keymap
@@ -93,6 +95,70 @@ local function get_pkg_path(pkg, path, opts)
   local ret = root .. "/packages/" .. pkg .. "/" .. path
   return ret
 end
+
+local function get_clients(opts)
+  local ret = {} ---@type vim.lsp.Client[]
+  if vim.lsp.get_clients then
+    ret = vim.lsp.get_clients(opts)
+  else
+    ---@diagnostic disable-next-line: deprecated
+    ret = vim.lsp.get_active_clients(opts)
+    if opts and opts.method then
+      ---@param client vim.lsp.Client
+      ret = vim.tbl_filter(function(client)
+        return client.supports_method(opts.method, { bufnr = opts.bufnr })
+      end, ret)
+    end
+  end
+  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+end
+
+local function format(opts)
+  opts = vim.tbl_deep_extend(
+    "force",
+    {},
+    opts or {},
+    require("nvim-lspconfig").format or {},
+    require("conform.nvim").format or {}
+  )
+  local ok, conform = pcall(require, "conform")
+  -- use conform for formatting with LSP when available,
+  -- since it has better format diffing
+  if ok then
+    opts.formatters = {}
+    conform.format(opts)
+  else
+    vim.lsp.buf.format(opts)
+  end
+end
+
+local function formatter(opts)
+  opts = opts or {}
+  local filter = opts.filter or {}
+  filter = type(filter) == "string" and { name = filter } or filter
+  local ret = {
+    name = "LSP",
+    primary = true,
+    priority = 1,
+    format = function(buf)
+      format(LazyUtil.merge({}, filter, { bufnr = buf }))
+    end,
+    sources = function(buf)
+      local clients = get_clients(LazyUtil.merge({}, filter, { bufnr = buf }))
+      ---@param client vim.lsp.Client
+      local ret = vim.tbl_filter(function(client)
+        return client.supports_method("textDocument/formatting")
+          or client.supports_method("textDocument/rangeFormatting")
+      end, clients)
+      ---@param client vim.lsp.Client
+      return vim.tbl_map(function(client)
+        return client.name
+      end, ret)
+    end,
+  }
+  return LazyUtil.merge(ret, opts)
+end
+
 return {
   opt = opt,
   autocmd = autocmd,
@@ -101,4 +167,6 @@ return {
   action = action,
   on_attach = on_attach,
   get_pkg_path = get_pkg_path,
+  get_clients = get_clients,
+  formatter = formatter,
 }
